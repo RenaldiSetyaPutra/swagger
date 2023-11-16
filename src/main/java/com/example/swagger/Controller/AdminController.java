885036d4fe57ecaa6123178b205f2ec67acee634
= <=>
2023-11-16 16:16:14 +0700

    change to swagger open api

--- src/main/java/com/example/swagger/Controller/AdminController.java
+++ src/main/java/com/example/swagger/Controller/AdminController.java
@@ -3,23 +3,67 @@ package com.example.swagger.Controller;
 import com.example.swagger.Pojo.Request.Request;
 import com.example.swagger.Pojo.Response.Response;
 import com.example.swagger.Pojo.SubModel.Details;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
-@Api(value = "Admin Controller", tags = "Admin Controller")
+//@Api(value = "Admin Controller", tags = "Admin Controller")
 public class AdminController {
+
+    @Operation(summary = "Get", description = "Get example Data")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "Success",
+                            content = {@Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Response.class))}
+                    ),
+                    @ApiResponse(
+                            responseCode = "400", description = "Bad Request",
+                            content = {@Content(mediaType = "application/json")}
+                    ),
+                    @ApiResponse(
+                            responseCode = "500", description = "Internal Server Error",
+                            content = {@Content(mediaType = "application/json")}
+                    )
+            }
+    )
     @GetMapping("/get")
-    @ApiOperation(value = "Get example Admin", notes = "Get example data from the API")
-    public String get(){
+//    @ApiOperation(value = "Get example Admin", notes = "Get example data from the API")
+    public String get() {
         return "Hello Admin";
     }
 
+    @Operation(summary = "Post", description = "Post example Data")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "Success",
+                            content = {@Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Response.class))}
+                    ),
+                    @ApiResponse(
+                            responseCode = "400", description = "Bad Request",
+                            content = {@Content(mediaType = "application/json")}
+                    ),
+                    @ApiResponse(
+                            responseCode = "500", description = "Internal Server Error",
+                            content = {@Content(mediaType = "application/json")}
+                    )
+            }
+    )
     @PostMapping("/post")
-    @ApiOperation(value = "Post example Admin", notes = "Post example data from the API")
-    public Response post(@RequestBody Request request){
+//    @ApiOperation(value = "Post example Admin", notes = "Post example data from the API")
+    public Response post(@RequestBody Request request) {
         Response response = new Response();
         Details details = new Details();
         details.setFullName("Your Full Name");
--- src/main/java/com/example/swagger/Controller/UserController.java
+++ src/main/java/com/example/swagger/Controller/UserController.java
@@ -3,24 +3,24 @@ package com.example.swagger.Controller;
 import com.example.swagger.Pojo.Request.Request;
 import com.example.swagger.Pojo.Response.Response;
 import com.example.swagger.Pojo.SubModel.Details;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-@Api(value = "User Controller", tags = "User Controller")
+//@Api(value = "User Controller", tags = "User Controller")
 public class UserController {
 
     @GetMapping("/get")
-    @ApiOperation(value = "Get example User", notes = "Post example data from the API")
+//    @ApiOperation(value = "Get example User", notes = "Post example data from the API")
 
     public String get(){
         return "Hello User";
     }
 
     @PostMapping("/post")
-    @ApiOperation(value = "Post example User", notes = "Post example data from the API")
+//    @ApiOperation(value = "Post example User", notes = "Post example data from the API")
     public Response post(@RequestBody Request request){
         Response response = new Response();
         Details details = new Details();
